7d0b46d6cfea96ad28a614cf4934afea9f756d7a
Gray Hound <admin@devilmaydie.name>
2019-05-23 17:20:18 +0500

    Fix circular errors list

--- src/BasicController.js
+++ src/BasicController.js
@@ -55,7 +55,7 @@ export default class BasicController {
       msg = this.fixInvalidErrorMessage(msg);
       const errorData = msg;
       errorData.param = error.param;
-      result.push(result, errorData);
+      result.push(errorData);
     });
     return result;
   }
